e8e07398e29919dfdcec508bf35caa32635733d0
Deborah <dnewberry16@gmail.com>
2017-03-09 04:20:56 +0000

    inital commit again

--- app.js
+++ app.js
@@ -4,29 +4,29 @@ var express = require("express"),
 var app = express();
 app.set("view engine", "ejs");
 
-// DATABASE
-mongoose.connect("mongodb://localhost/personal_site");
-var Project = mongoose.model("Project", new mongoose.Schema({
-    name: String,
-    images: [String],
-    completed: String,
-    language: String,
-    platform: String,
-    gitURL: String,
-    description: String
-}));
+// // DATABASE
+// mongoose.connect("mongodb://localhost/personal_site");
+// var Project = mongoose.model("Project", new mongoose.Schema({
+//     name: String,
+//     images: [String],
+//     completed: String,
+//     language: String,
+//     platform: String,
+//     gitURL: String,
+//     description: String
+// }));
 
-var ResumeItem = mongoose.model("ResumeItem", new mongoose.Schema({
-    title: String,
-    date: String,
-    description: String
-}));
+// var ResumeItem = mongoose.model("ResumeItem", new mongoose.Schema({
+//     title: String,
+//     date: String,
+//     description: String
+// }));
 
-var ResumeSkill = mongoose.model("ResumeSkill", new mongoose.Schema({
-    title: String,
-    description: String,
-    isExperience: Boolean
-}));
+// var ResumeSkill = mongoose.model("ResumeSkill", new mongoose.Schema({
+//     title: String,
+//     description: String,
+//     isExperience: Boolean
+// }));
 
 // INDEX
 app.get("/", function(req, res) {
