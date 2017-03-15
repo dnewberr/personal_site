9751fc5d63a1da9c66f632d0d98e0de8f2a9654e
Deborah <dnewberry16@gmail.com>
2017-03-15 02:04:59 +0000

    added checking for mobile in contact form, fixed some spacing on mobile

--- public/contact.js
+++ public/contact.js
@@ -7,6 +7,24 @@ $(document).ready(function () {
         }
     });
 
+    $('#contact-me-form').submit(function(){
+        var required = $('[required="required"]'); // change to [required] if not using true option as part of the attribute as it is not really needed.
+        var error = false;
+
+
+        for(var i = 0; i <= (required.length - 1); i++) {
+            if(required[i].value == '') { // tests that each required value does not equal blank, you could put in more stringent checks here if you wish.
+                console.log("EXTRA JQUERY FOR "  + required[i])
+                required[i].style.borderColor = 'rgb(255,155,155)';
+                error = true; // if any inputs fail validation then the error variable will be set to true;
+            }
+        }
+
+        if(error) { // if error is true;
+            return false; // stop the form from being submitted.
+        }
+    });
+
     function check_form(){
         var flag = true;
 
